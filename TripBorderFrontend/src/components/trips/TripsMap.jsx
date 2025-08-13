3fb4a91d9a2eee11b963b6715fd10718922241f2
Nientai Ho <hntddt123@gmail.com>
2025-08-13 13:31:56 +0800

    Improved small details

--- TripBorderFrontend/src/components/trips/TripsMap.jsx
+++ TripBorderFrontend/src/components/trips/TripsMap.jsx
@@ -1,6 +1,11 @@
+import { useEffect } from 'react';
 import CustomMap from './CustomMap';
 
 export default function TripsMap() {
+  useEffect(() => {
+    setTimeout(() => window.scrollTo({ top: 30, behavior: 'smooth' }), 100);
+  }, []);
+
   return (
     <CustomMap />
   );
