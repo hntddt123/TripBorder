d66bb4db498fdd3757f04de167b4264751451d1e
Nientai Ho <hntddt123@gmail.com>
2026-05-04 13:08:38 +0800

    Fixed namedetails null

--- TripBorderFrontend/src/utility/osmFormat.js
+++ TripBorderFrontend/src/utility/osmFormat.js
@@ -1,2 +1,2 @@
 export const getOSMAddress = (result) => result.display_name;
-export const getAltName = (marker) => (marker.namedetails.alt_name ? `(${marker.namedetails.alt_name})` : '');
+export const getAltName = (marker) => (marker.namedetails?.alt_name ? `(${marker.namedetails.alt_name})` : '');
