95b521c989ba78c312c83ec3d9c1e6852f830868
Nientai Ho <hntddt123@gmail.com>
2026-08-12 22:10:00 +0800

    Updated UI for TripCountriesOptions

--- TripBorderFrontend/src/components/trips/mapControls/TripCountriesOptions.jsx
+++ TripBorderFrontend/src/components/trips/mapControls/TripCountriesOptions.jsx
@@ -1,25 +1,41 @@
+import { useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSearchCountry } from '../../../redux/reducers/userSettingsReducer';
 import { COUNTRIES_BOUNDING_BOX } from '../../../constants/constants';
 
 export default function TripCountriesOptions() {
+  const [isOnlyEmoji, setIsOnlyEmoji] = useState(false);
   const { searchCountry } = useSelector((state) => state.userSettingsReducer);
   const { searchKeyword } = useSelector((state) => state.mapReducer);
   const dispatch = useDispatch();
+  const ref = useRef();
 
   const getSearchCountry = () => Object.entries(COUNTRIES_BOUNDING_BOX)
     .map(([code, country]) => (
       <option key={code} value={code}>
-        {country.name} {country.emoji}
+        {isOnlyEmoji ? `${country.emoji}` : `${country.name} ${country.emoji}`}
       </option>
     ));
 
   const handleSearchCountryChange = (e) => {
     dispatch(setSearchCountry(e.target.value));
+    setIsOnlyEmoji(true);
+    if (!isOnlyEmoji) ref.current.blur();
+  };
+
+  const handleFocus = () => {
+    setIsOnlyEmoji(false);
+  };
+
+  const handleBlur = () => {
+    setIsOnlyEmoji(true);
   };
 
   return (
     <select
+      ref={ref}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
       className='clearButton select-none max-w-14 max-h-8 text-sm'
       id='language'
       name='language'
